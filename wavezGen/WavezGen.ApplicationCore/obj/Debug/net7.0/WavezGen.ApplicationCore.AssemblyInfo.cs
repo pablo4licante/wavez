a2f78d3ae12ff5b67ae7abf9007e8e50d37f44e1
Pablo Alicante <pabloalicanteat@hotmail.com>
2024-11-06 18:24:47 +0100

    deleted Editar Perfil / alredy implemented in modificar

--- wavezGen/WavezGen.ApplicationCore/obj/Debug/net7.0/WavezGen.ApplicationCore.AssemblyInfo.cs
+++ wavezGen/WavezGen.ApplicationCore/obj/Debug/net7.0/WavezGen.ApplicationCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WavezGen.ApplicationCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb7b180b2f33c3e1a1e0fca252f1189feae04b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdc97ba144743d3ae150472df8815afa2a8208d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WavezGen.ApplicationCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WavezGen.ApplicationCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
