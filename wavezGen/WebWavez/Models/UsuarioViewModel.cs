393c96e932930448dccf88574fa53c0a24d9cb7b
Sara Navarro Más <snm33@mscloud.ua.es>
2024-12-05 12:26:05 +0100

    login

--- wavezGen/WebWavez/Models/UsuarioViewModel.cs
+++ wavezGen/WebWavez/Models/UsuarioViewModel.cs
@@ -1,27 +1,68 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-
+using System.Linq;
+using System.Threading.Tasks;
 namespace WebWavez.Models
 {
+
     public class UsuarioViewModel
     {
         [ScaffoldColumn(false)]
+
         public string Usuario { get; set; }
 
-        [Display(Prompt = "Nombre del usuario", Description = "Nombre del usuario", Name = "Nombre")]
-        [Required(ErrorMessage = "Debe indicar un nombre para el usuario")]
-        [StringLength(maximumLength: 50, ErrorMessage = "El nombre del usuario no puede tener más de 50 caracteres")]
         public string Nombre { get; set; }
 
-        [Display(Prompt = "Contraseña del usuario", Description = "Contraseña del usuario", Name = "Contraseña")]
-        [Required(ErrorMessage = "Debe indicar una contraseña para el usuario")]
-        [StringLength(maximumLength: 50, ErrorMessage = "La contraseña del usuario no puede tener más de 50 caracteres")]
-        public string Contrasenya { get; set; }
+        public string Password { get; set; }
 
-        [Display(Prompt = "Email del usuario", Description = "Email del usuario", Name = "Email")]
-        [Required(ErrorMessage = "Debe indicar un email para el usuario")]
-        [EmailAddress(ErrorMessage = "Debe ser una dirección de email válida")]
         public string Email { get; set; }
 
         public string FotoPerfil { get; set; }
+
+    }
+
+    public class LoginUsuarioViewModel
+    {
+        [Display(Prompt = "Introduce el nombre de usuario", Description = "Nombre de usuario de Usuario", Name = "Usuario *")]
+        [Required(ErrorMessage = "El nombre de usuario es obligatorio")]
+        public string Usuario { get; set; }
+
+        [Display(Prompt = "Introduce la contraseña", Description = "Password del Usuario", Name = "Password *")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        public string Password { get; set; }
+
+    }
+
+
+    public class RegistroUsuarioViewModel
+    {
+        [Display(Prompt = "Introduce el nombre de usuario", Description = "Nombre de usuario de Usuario", Name = "Usuario *")]
+        [Required(ErrorMessage = "El nombre de usuario es obligatorio")]
+        public string Usuario { get; set; }
+
+        [Display(Prompt = "Introduce tu nombre", Description = "Nombre del Usuario", Name = "Nombre *")]
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Display(Prompt = "Introduce la contraseña", Description = "Password del Usuario", Name = "Password *")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        public string Password { get; set; }
+
+        [Display(Prompt = "Confirma la contraseña", Description = "Confirmar la password del Usuario", Name = "Confirmar *")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Repetir la contraseña es obligatorio")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Prompt = "Introduce el email", Description = "Email del Usuario", Name = "Email *")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "El email es obligatorio")]
+        public string Email { get; set; }
+
+        [Display(Prompt = "Introduce tu foto de perfil", Description = "Foto de perfil del Usuario", Name = "Foto *")]
+        public string FotoPerfil { get; set; }
+
     }
 }
